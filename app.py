8cad338a2eb7061f86198b108485b5e170daf207
athornton3 <42791680+athornton3@users.noreply.github.com>
2023-04-08 14:05:36 -0500

    indent issue

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def search_projects(title, abstract):
     for prj in results[0]:
         related_project = projects_df.loc[prj['corpus_id']]
         df.append(related_project)
-	res = pd.concat(df)
+    res = pd.concat(df)
     return res 
 
 try:
