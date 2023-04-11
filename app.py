6060b8954dc9092ed984a3ececee6d51a4050f52
athornton3 <42791680+athornton3@users.noreply.github.com>
2023-04-11 17:45:53 -0500

    have the right dict, not sure about append

--- app.py
+++ app.py
@@ -126,11 +126,11 @@ for event in resp['Payload']:
 	if 'Records' in event:
 		records = event['Records']['Payload'].decode('utf-8')
 		df = pd.read_csv(io.StringIO(records), sep=",")
-		st.write(dict(zip(columns.columns, [col for col in df])))
+		df = df.append(dict(zip(columns.columns, [col for col in df]))))
 		#df.append(dict(zip(df.columns, [col for col in df])), ignore_index=True)
 		#hold = columns.columns.to_list()
 		#hold = hold.insert(0,"test")
-		df.columns = columns.columns.to_list()
+		#df.columns = columns.columns.to_list()
 		#df = pd.concat([df, list],ignore_index = True)
 		st.dataframe(df)
 	elif 'Stats' in event:
