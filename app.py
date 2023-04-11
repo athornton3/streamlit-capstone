e94e0373421e2d2953e68fefe94aa1b31c92d00c
athornton3 <42791680+athornton3@users.noreply.github.com>
2023-04-11 16:59:28 -0500

    another try at adding columns

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ for event in resp['Payload']:
 	if 'Records' in event:
 		records = event['Records']['Payload'].decode('utf-8')
 		df = pd.read_csv(io.StringIO(records), sep=",")
-		#df.columns = columns.loc[0, :].values.flatten().tolist() 
+		df.columns = columns 
 		#df = pd.concat([df, list],ignore_index = True)
 		st.dataframe(df)
 	elif 'Stats' in event:
