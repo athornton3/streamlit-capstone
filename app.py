e484c4574ff78e20667bd72b25ca151ad3daee57
athornton3 <42791680+athornton3@users.noreply.github.com>
2023-04-11 16:39:29 -0500

    switch to concat for columnnames

--- app.py
+++ app.py
@@ -126,7 +126,8 @@ for event in resp['Payload']:
 	if 'Records' in event:
 		records = event['Records']['Payload'].decode('utf-8')
 		df = pd.read_csv(io.StringIO(records), sep=",")
-		df.columns = columns
+		#df.columns = columns
+		df = pd.concat([df, columns],ignore_index = True)
 		st.dataframe(df)
 	elif 'Stats' in event:
 		statsDetails = event['Stats']['Details']
