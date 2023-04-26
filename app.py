bbf031b30100c82c13f50722ed04c9b81a56b215
athornton3 <42791680+athornton3@users.noreply.github.com>
2023-04-26 08:58:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ st.set_page_config(
 	page_icon="🐙",
 )
 
-st.title("Find Related Research Info 🐙")
+st.title("Find Related Research Info 🔍")
 
-with st.expander("ℹ️ - About this app", expanded=False):
+with st.expander("ℹ️ - About this app - DS785 Capstone SP23SEC01", expanded=False):
     st.write(
         """ -   This app uses embedding created with SPECTER and awarded NSF grants from 2023. Paste and get matching projects and more info """
     )
@@ -34,7 +34,7 @@ with st.expander("ℹ️ - About this app", expanded=False):
 
 #Create input form for title and abstract 
 with st.form("form1", clear_on_submit=False): 
-    c1, c2 = st.columns([1,2], gap="small")
+    c1, c2 = st.columns([1,2], gap="large")
     with c1:
         title = st.text_input("Title") 
         abstract = st.text_area("Abstract (max 300 words)",
@@ -46,25 +46,26 @@ with st.form("form1", clear_on_submit=False):
         if res > MAX_WORDS:
             st.warning("⚠️ Your text contains " + str(res) + " words." + " Only the first 300 words will be reviewed.")
             abstract = abstract[:MAX_WORDS]
-    
+        
+        includeConf = st.checkbox('Include Conference Awards', value=True)
+        
         numResults = st.slider(
-            "Number of results",
+            "Max number of results",
             min_value=3,
-            max_value=30,
+            max_value=50,
             value=10,
             help="You can choose the number of results to display. Between 3 and 30, default number is 10.",
             )
         
-        dollarThreshold = st.slider(
-            "Awarded Amount",
-            min_value=0,
-            max_value=10000000,
-            value=0,
-            help="You can choose the minimum dollar threshold for results. Between $0 and $10million, default is $0.",
-            )
+        #dollarThreshold = st.slider(
+        #    "Awarded Amount",
+        #    min_value=0,
+        #    max_value=10000000,
+        #    value=0,
+        #    help="You can choose the minimum dollar threshold for results. Between $0 and $10million, default is $0.",
+        #    )
             
-        includeConf = st.checkbox('Include Conference Awards', value=True)
-			
+        
         submit_button = st.form_submit_button("Submit title/abstract")
 
 if not submit_button:
@@ -81,12 +82,6 @@ def read_file(filename):
 def load_model(tf):
 	return SentenceTransformer(tf)
 
-# create embeddings - slow and resource intensive
-@st.cache_data
-def get_embeddings(_model, data):
-	embeddings = model.encode(data, convert_to_tensor=True)
-	return embeddings
-
 # read stored embeddings fom s3
 @st.cache_data
 def read_embeddings(filename):
@@ -94,67 +89,19 @@ def read_embeddings(filename):
         cache_data = pickle.loads(pkl.read())
     return cache_data
 
-projects_df = read_file("streamlitbucketcapstoneajt/export_2023_col_rv_latlong.csv")
+#projects_df = read_file("streamlitbucketcapstoneajt/export_2023_col_rv_latlong_narrow_topics.csv")
+projects_df = pd.read_csv("export_2023_col_rv_latlong_narrow_topics.csv")
 project_texts = projects_df['AwardTitle'].astype(str) + '[SEP]' + projects_df['AbstractNarration'].astype(str)
-
-embeddings = read_embeddings("streamlitbucketcapstoneajt/corpus_embeddings_2023.pkl")
-#embeddings = get_embeddings(model, project_texts) #only if embeddings not found/not available
-
 model = load_model('allenai-specter')
 
-s3 = boto3.client('s3')
-
-#select column headers
-resp = s3.select_object_content(
-    Bucket='streamlitbucketcapstoneajt',
-    Key='export_2023_col_rv_latlong.csv',
-    ExpressionType='SQL',
-    Expression="SELECT * FROM s3object s limit 1",
-    InputSerialization = {'CSV': {"FileHeaderInfo": "None"}, 'CompressionType': 'NONE'},
-    OutputSerialization = {'CSV': {}},
-)
-for event in resp['Payload']:
-	if 'Records' in event:
-		records = event['Records']['Payload'].decode('utf-8')
-		columns = pd.read_csv(io.StringIO(records), sep=",")
-	elif 'Stats' in event:
-		statsDetails = event['Stats']['Details']
-		#st.write("Stats details bytesScanned: "+str(statsDetails['BytesScanned']))
-
-#don't have to do it this way as the csv is pretty small and embeddings small too
-resp = s3.select_object_content(
-    Bucket='streamlitbucketcapstoneajt',
-    Key='export_2023_col_rv_latlong.csv',
-    ExpressionType='SQL',
-    Expression="SELECT * FROM s3object s where s.\"Index0\" in ('10','80')",
-    InputSerialization = {'CSV': {"FileHeaderInfo": "Use"}, 'CompressionType': 'NONE'},
-    OutputSerialization = {'CSV': {}},
-)
-
-#resp = s3.select_object_content(
-#    Bucket='streamlitbucketcapstoneajt',
-#    Key='export_21_22_23_col_rv_100_latlong.csv',
-#    ExpressionType='SQL',
-#    Expression="SELECT * FROM s3object s where s.\"AwardTitle\" = 'CI CoE: Demo Pilot: Advancing Research Computing and Data: Strategic Tools, Practices, and Professional Development'",
-#    InputSerialization = {'CSV': {"FileHeaderInfo": "Use"}, 'CompressionType': 'NONE'},
-#    OutputSerialization = {'CSV': {}},
-#)
-
-for event in resp['Payload']:
-	if 'Records' in event:
-		records = event['Records']['Payload'].decode('utf-8')
-		df = pd.read_csv(io.StringIO(records), sep=",")
-		#st.write(dict(zip(df.columns, [col for col in df])))
-		df = df.append(dict(zip(df.columns, [col for col in df])), ignore_index=True) 
-		df.columns = columns.columns.to_list()
-		#st.dataframe(df)
-	elif 'Stats' in event:
-		statsDetails = event['Stats']['Details']
-		#st.write("Stats details bytesScanned: "+str(statsDetails['BytesScanned']))
-#sentences_array = project_texts.to_numpy()
-			
+#embeddings = read_embeddings("streamlitbucketcapstoneajt/corpus_embeddings_2023.pkl")
+@st.cache_data
+def get_embeddings(_model, data):
+	embeddings = model.encode(data, convert_to_tensor=True)
+	return embeddings
+embeddings = get_embeddings(model, project_texts) #only if embeddings not found/not available
+#pickle.dump(embeddings, fs.open(f"streamlitbucketcapstoneajt/corpus_embeddings_2023.pkl",'wb'))
 #function to take title & abstract and search corpus for similar projects
-@st.cache_data(max_entries=1)
 def search_projects(title, abstract, n):
     query_embedding = model.encode(title+'[SEP]'+abstract, convert_to_tensor=True)
 
@@ -163,33 +110,26 @@ def search_projects(title, abstract, n):
     df = pd.DataFrame()
     scores = pd.DataFrame()
     for prj in results[0]:
-        related_project = projects_df.loc[projects_df["Index0"] == prj['corpus_id']]
-        scores = scores.append({"Index0" : prj['corpus_id'], "cosim" : prj['score']}, ignore_index=True)
+        related_project = projects_df.loc[projects_df["Unnamed: 0"] == prj['corpus_id']]
+        scores = scores.append({"Unnamed: 0" : prj['corpus_id'], "cosim" : prj['score']}, ignore_index=True)
         df = df.append(related_project) #deprecated but couldn't get pd.concat to work
-    st.dataframe(df)  
-    df2 = scores.merge(df, on="Index0")  
-    st.write(scores)
+    #st.dataframe(df)  
+    df2 = scores.merge(df, on="Unnamed: 0")  
+    #st.write(scores)
     return df2
-
-def search_projects_sql(title, abstract, n):
-    query_embedding = model.encode(title+'[SEP]'+abstract, convert_to_tensor=True)
-
-    results = util.semantic_search(query_embedding, embeddings, top_k = n)
-    results_normalized = util.semantic_search(query_embedding, embeddings, score_function=util.dot_score, top_k = n)
-    df = pd.DataFrame()
-    scores = []
-    award_index = []
-    for prj in results[0]:
-        award_index.append(prj['corpus_id'])
-        scores.append(prj['score'])
-    #df.insert(0, "cosim_score", scores)
-    #st.write(award_index)
-    return df
 
 try:
     df = search_projects(title, abstract, numResults)
-    search_projects.clear()
-    st.dataframe(df[['Index0', 'cosim', 'AwardTitle', 'AbstractNarration', 'AwardAmount', 'AwardEffectiveDate', 'Organization-Directorate-LongName','Organization-Division-LongName','Institution-Name','Investigator-PI_FULL_NAME']])
+    #st.dataframe(df)
+    display_df = df[['Unnamed: 0', 'cosim', 'Name', 'AwardTitle', 'AbstractNarration', 'AwardID', 'AwardAmount', 'AwardEffectiveDate', 'Organization-Directorate-LongName','Organization-Division-LongName','Institution-Name','Investigator-PI_FULL_NAME', 'ProgramElement-Text']]
+    if includeConf:
+        new_df = display_df
+    else:
+        new_df = display_df[~display_df["AbstractNarration"].str.contains('conference', na=False)]
+        
+    new_df.rename(columns = {'Unnamed: 0': "Index", 'cosim':'Similarity', 'Name':'Topic', 'AwardTitle':
+    'Award Title', 'AbstractNarration':'Abstract', 'AwardAmount':'Award Amt', 'Organization-Directorate-LongName':'NSF Directorate','Organization-Division-LongName':'NSF Division','ProgramElement-Text': 'Funding Program', 'Institution-Name':'Institution','Investigator-PI_FULL_NAME':'PI'}, inplace = True) 
+    st.dataframe(new_df)
     with c2: # Map demo
         map_data = df[['latitude','longitude','Institution-Name','Investigator-PI_FULL_NAME']]
         map_data['Institution'] = map_data['Institution-Name'] + '<br/><b>PI:</b> ' + map_data['Investigator-PI_FULL_NAME']
@@ -197,8 +137,9 @@ try:
         st.subheader('Matching Research Institutions')
 	
         m = leafmap.Map(center=(39.381266, -97.922211), zoom=4)
-        m.add_circle_markers_from_xy(map_data, x="longitude", y="latitude", radius=5, popup='Institution') #min-width and max-width for the popup
-        #add_text and add_legend https://leafmap.org/foliumap/#leafmap.foliumap.Map.add_legend
+        m.add_circle_markers_from_xy(map_data, x="longitude", y="latitude", radius=5, popup='Institution')
+        m.add_text(text="Map of Institutions", position="bottomright")
+        
         folium_static(m)
 		
 except URLError as e:
